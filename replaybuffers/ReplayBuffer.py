24bb8b5aa13d36d59f1bc0be7cdf6076d525b5a7
DibyojyotiS <dibyojyoti21century@gmail.com>
2021-12-16 13:13:31 +0530

    updated doc-strings

--- replaybuffers/ReplayBuffer.py
+++ replaybuffers/ReplayBuffer.py
@@ -4,20 +4,31 @@ import torch
 
 
 class ReplayBuffer:
+    """ Base Class for replay buffers """
 
     def __init__(self) -> None:
         pass
 
     def store(self, *args, **kwargs):
+        """ stores the experience in the buffer. """
         raise NotImplementedError()
 
 
     def sample(self, batchSize, **kwargs):
+        """ samples a batchSize number of experiences and returns a dict as 
+
+        sample = {  'state':tf.Tensor, 'action':tf.Tensor, 'reward':tf.Tensor, 
+                    'nextState':tf.Tensor, 'done':tf.Tensor} 
+
+        you can return something else but then must modify the training 
+        algorithm. """
         raise NotImplementedError()
 
     
     def update(self, *args, **kwargs):
-        pass # to be implemented only if updating is required
+        """ to be implemented only if updating something at every gradient step
+        is required """
+        pass
 
 
     def update_params(self):
@@ -31,7 +42,8 @@ class ReplayBuffer:
         """ inits the buffer as a dict with the keys as in _tuppleDesc and the values
         as torch.empty tensor of length bufferSize and correct dimensions. The given experience
         is a list of tensors. And is used to infer the dimentions and the devices of the tensors.
-        NOTE: Each element of the list experiece must be torch tensors """
+        NOTE: Each element of the list experiece must be torch tensors 
+        NOTE: requires_grad=False is assumed """
         return {x: torch.empty(size=(self.bufferSize, *experience[i].shape), 
                                         dtype=experience[i].dtype,
                                         requires_grad=False,
