b05d2ee9c7244fea174161eae264dcd0d6420712
DibyojyotiS <dibyojyoti21century@gmail.com>
2021-12-26 12:58:01 +0530

    minor edit

--- examples/VPGexample.py
+++ examples/VPGexample.py
@@ -14,24 +14,21 @@ env = gym.make('CartPole-v0')
 # pick a suitable device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# create the value network, i know duelling net is not required here but... :p
-class duellingDNN(nn.Module):
+# create the value network
+class valueDNN(nn.Module):
     def __init__(self, inDim, outDim, hDim, activation = F.relu):
-        super(duellingDNN, self).__init__()
+        super(valueDNN, self).__init__()
         self.inputlayer = nn.Linear(inDim, hDim[0])
         self.hiddenlayers = nn.ModuleList([nn.Linear(hDim[i], hDim[i+1]) for i in range(len(hDim)-1)])
         self.valuelayer = nn.Linear(hDim[-1], 1)
-        self.actionadv = nn.Linear(hDim[-1], outDim)
         self.activation = activation
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.activation(self.inputlayer(x))
         for layer in self.hiddenlayers:
             x = self.activation(layer(x))
-        advantages = self.actionadv(x)
-        values = self.valuelayer(x)
-        qvals = values + (advantages - advantages.mean())
-        return qvals
+        value = self.valuelayer(x)
+        return value
 
 # create the policy network
 class net(nn.Module):
@@ -52,8 +49,10 @@ class net(nn.Module):
 
 
 # init necessities
-value_model = duellingDNN(inDim=4, outDim=1, hDim=[8,8], activation=F.relu).to(device)
+value_model = valueDNN(inDim=4, outDim=1, hDim=[8,8], activation=F.relu).to(device)
 policy_model = net(inDim=4, outDim=2, hDim=[8,8], activation=F.relu).to(device)
+
+# init necessities
 policyOptimizer = optim.Adam(policy_model.parameters(), lr=0.01)
 valueOptimizer = optim.Adam(value_model.parameters(), lr=0.01)
 trainExplortionStrategy = softMaxAction(policy_model, outputs_LogProbs=True)
