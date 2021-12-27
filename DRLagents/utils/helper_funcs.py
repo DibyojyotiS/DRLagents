b7b1970a8a84ec82989f75c0fad4b1dc659ee62e
DibyojyotiS <dibyojyoti21century@gmail.com>
2021-12-28 00:38:51 +0530

    upload files

--- DRLagents/utils/helper_funcs.py
+++ DRLagents/utils/helper_funcs.py
@@ -1,4 +1,13 @@
 import numpy as np
+import torch
+import random
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
 
 def movingAverage(array, n=5):
     array = np.cumsum(array)
--- examples/comparison.py
+++ examples/comparison.py
@@ -1,7 +1,10 @@
 # being implemented
+from DRLagents.utils.helper_funcs import set_seed
 from examples.DQNexample import run_DQN_on_cartpole_V0
 from examples.VPGexample2 import run_VPG_on_cartpole_V0
 from examples.helper_funcs.helper_funcs import run_big_experiment
+
+set_seed(0)
 
 if __name__ == "__main__":
     VPGresults = run_big_experiment(run_VPG_on_cartpole_V0, 5, 
