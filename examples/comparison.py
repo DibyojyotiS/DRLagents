15b96b6bc4ef9182d526ffbbf3eaf6fc570eb7b4
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-01-09 16:58:08 +0530

    playing with VPG

--- examples/comparison.py
+++ examples/comparison.py
@@ -1,10 +1,11 @@
 # being implemented
 from DRLagents.utils.helper_funcs import set_seed
-from examples.D3QNexample import run_D3QN_on_cartpole_V0
-from examples.DQNexample import run_DQN_on_cartpole_V0
-from examples.VPGexample2 import run_VPG_on_cartpole_V0
 from examples.helper_funcs.bigExperiment import run_big_experiment
 
+from examples.D3QNexample import run_D3QN_on_cartpole_V0
+from examples.DQNexample import run_DQN_on_cartpole_V0
+# from examples.VPGexample import run_VPG_on_cartpole_V0
+from examples.VPGexample2 import run_VPG_on_cartpole_V0
 
 if __name__ == "__main__":
     VPGresults = run_big_experiment(run_VPG_on_cartpole_V0, 5, 
