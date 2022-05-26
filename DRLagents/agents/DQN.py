8972510431c4027498b34b6d6b5040ebec5bc4c7
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-26 20:48:39 +0530

    minor updates

--- DRLagents/agents/DQN.py
+++ DRLagents/agents/DQN.py
@@ -541,7 +541,8 @@ class DQN:
         """ attempt to load the replayBuffer, optimizer, trainExplorationStrategy,
         episode-number, online-model and the target-model to resume the training. """
 
-        print('Attempting resume ...', end=' ')
+        if not os.path.exists(self.log_dir): return
+        else: print('Attempting resume ...', end=' ')
 
         def load_csvbook(path):
             with open(path, 'r') as f:
