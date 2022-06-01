6973b5cf101d8b9cbecf55637dde8852adc75a9e
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-06-01 14:15:12 +0530

    docstring update, GAE func updated

--- DRLagents/agents/common_helper_funcs/helper_funcs.py
+++ DRLagents/agents/common_helper_funcs/helper_funcs.py
@@ -10,9 +10,6 @@ def polyak_update(online_model:nn.Module, target_model:nn.Module, tau=0.1):
         for key in online_stateDict:
             target_stateDict[key] = (1 - tau)*target_stateDict[key] + tau*online_stateDict[key]
         target_model.load_state_dict(target_stateDict)
-        # for paramOnline, paramTarget in zip(online_model.parameters(), target_model.parameters()):
-        #     paramTarget.data = tau * paramOnline.data + (1 - tau) * paramTarget.data
-
 
 def clip_grads(model:nn.Module, _min=-1.0, _max=1.0):
     for param in model.parameters():
@@ -21,21 +18,28 @@ def clip_grads(model:nn.Module, _min=-1.0, _max=1.0):
 
 
 # GAE estimate
-def compute_GAE(values:Tensor, rewards, gamma:float, lamda:float):
+def compute_GAE(values:Tensor, rewards:list, dones:list, gamma:float, lamda:float):
     '''
-    value_estimates: values from value_model on the trajectory
-    rewards: the rewards encountered in the trajectory
-    gamma: discount factor
-    lamda: i danced on the moon
-    Assumes that the last value belongs to a terminal state
+    This computes the Generalized Advantage Estimate
+    ### parameters
+    1. value_estimates: Tensor
+            - values from value_model on the trajectory
+    2. rewards: list[float]
+            - the rewards encountered in the trajectory
+    3. gamma: float
+            - the discount factor
+    4. lamda: float
+            - used to interpolate between 1-step TD
+            and n-step TD
+
+    ### returns
+    - gae: Generalized Advantage Estimate
     '''
 
-    T = len(rewards) # last state is terminal
-
+    T = len(rewards)
     gae = torch.zeros_like(values)
-    
     for t in reversed(range(T)):
-        delta = rewards[t] + gamma*(t!=T-1)*values[t+1] - values[t]
+        delta = rewards[t] + gamma*(not dones[t])*values[t+1] - values[t]
         gae[t] = delta + gamma*lamda*gae[t+1]*(t!=T-1)
 
     return gae
@@ -45,28 +49,40 @@ def default_make_transitions(trajectory, state, action, nextState):
     '''
     Usefull for frame-skipping, where the SAME action is repeated for a small number of steps
     
-    trajectory: which is a list of [next-observation, info, reward, done]
-    state: the state before the begenning of the frame-skipping
-    action: the action repeated for frame-skipping
-    nextState: the state after the frame-skipping
+    ### parameters
+    1. trajectory: list
+            - which is a list of [next-observation, info, reward, done]
+            - this is the trajectory while skipping-frames
+            - all the observtions, infos, etc are those of the skipped frames
+    2. state: 
+            - the state before the beginning of the frame-skipping
+    3. action: 
+            - the action repeated for frame-skipping
+    4. nextState: 
+            - the state after the frame-skipping
     
-    returns:
-    transitions: list of state-transitions of form [state, action, reward, next-state, done]
+    ### returns:
+        - transitions: list
+                - state-transitions of form [state, action, reward, next-state, done]
     '''
     reward = sum([r for o,i,r,d in trajectory])
     done = trajectory[-1][-1]
     return [[state, action, reward, nextState, done]]
 
 
-def default_make_state(trajectory, action_taken):
+def default_make_state(trajectory:list, action_taken):
     '''
     Usefull for frame-skipping, where the SAME action is repeated for a small number of steps
     
-    trajectory: which is a list of [next-observation, info, reward, done]
-    state: the state before the begenning of the frame-skipping
-    action: the action repeated for frame-skipping
-    nextState: the state after the frame-skipping
+    ### parameters
+    1. trajectory: list
+            - which is a list of [next-observation, info, reward, done]
+            - this is the trajectory while skipping-frames
+            - all the observtions, infos, etc are those of the skipped frames
+    2. action_taken:
+            - the action repeated for frame-skipping
     
-    returns: next-state: a tensor
+    ### returns
+        - next-state: tensor
     ''' 
     return trajectory[-1][0]
